3a337549903389c7b74c7ac5d0ecc84c5c5c04a2
DESKTOP-EA3UTGO\ujjwal <ujjwalkumargupta4u@gmail.com>
2018-10-15 08:57:20 +0530

    added react example

---
+++ examples/react/src/service/idb_service.js
@@ -0,0 +1,7 @@
+import * as JsStore from "jsstore";
+import * as SqlWeb from "sqlweb";
+const Worker = require("worker-loader?publicPath=/&name=jsstore.worker.js!jsstore/dist/jsstore.worker.min");
+
+// This will ensure that we are using only one instance. 
+// Otherwise due to multiple instance multiple worker will be created.
+export const idbCon = new JsStore.Instance(new Worker());
