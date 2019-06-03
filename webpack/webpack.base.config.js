8f02f9b4100b84216617538aeba2fb2108114d07
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2019-06-03 08:54:48 +0530

    moved build codes to build_helper

--- webpack/webpack.base.config.js
+++ webpack/webpack.base.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const SmartBannerPlugin = require('smart-banner-webpack-plugin');
-const banner = require('../license');
+const banner = require('../build_helper/license');
 
 module.exports = [{
     name: "sqlweb",
