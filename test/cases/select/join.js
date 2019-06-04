850836390fcd468c174f9577dba3b3c47a30cce3
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2019-06-04 11:28:31 +0530

    three table join test case added

--- test/cases/select/join.js
+++ test/cases/select/join.js
@@ -13,25 +13,26 @@ describe('Test join', function () {
         //         }
         //     }
         // })
-        con.runSql(`select * from Orders join Customers on Orders.CustomerID=Customers.CustomerID`).then(function (results) {
-            expect(results).to.be.an('array').length(196);
-            // const firstValue = results[0];
-            // console.log(firstValue);
-            // expect(firstValue).to.be.an('object').to.haveOwnProperty('name');
-            // expect(firstValue).to.be.an('object').to.haveOwnProperty('cName');
-            // expect(firstValue).to.be.an('object').to.haveOwnProperty('OrderID');
-            // expect(firstValue).to.be.an('object').to.haveOwnProperty('CustomerID');
-            // expect(firstValue).to.be.an('object').to.haveOwnProperty('EmployeeID');
-            // expect(firstValue).to.be.an('object').to.haveOwnProperty('cId');
-            // expect(firstValue).to.be.an('object').to.haveOwnProperty('ShipperID');
-            // expect(firstValue).to.be.an('object').to.haveOwnProperty('Address');
-            // expect(firstValue).to.be.an('object').to.haveOwnProperty('City');
-            // expect(firstValue).to.be.an('object').to.haveOwnProperty('PostalCode');
-            // expect(firstValue).to.be.an('object').to.haveOwnProperty('Country');
-            done();
-        }).catch(function (err) {
-            done(err);
-        })
+        con.runSql(`select * from Orders join Customers on Orders.CustomerID=Customers.CustomerID`)
+            .then(function (results) {
+                expect(results).to.be.an('array').length(196);
+                // const firstValue = results[0];
+                // console.log(firstValue);
+                // expect(firstValue).to.be.an('object').to.haveOwnProperty('name');
+                // expect(firstValue).to.be.an('object').to.haveOwnProperty('cName');
+                // expect(firstValue).to.be.an('object').to.haveOwnProperty('OrderID');
+                // expect(firstValue).to.be.an('object').to.haveOwnProperty('CustomerID');
+                // expect(firstValue).to.be.an('object').to.haveOwnProperty('EmployeeID');
+                // expect(firstValue).to.be.an('object').to.haveOwnProperty('cId');
+                // expect(firstValue).to.be.an('object').to.haveOwnProperty('ShipperID');
+                // expect(firstValue).to.be.an('object').to.haveOwnProperty('Address');
+                // expect(firstValue).to.be.an('object').to.haveOwnProperty('City');
+                // expect(firstValue).to.be.an('object').to.haveOwnProperty('PostalCode');
+                // expect(firstValue).to.be.an('object').to.haveOwnProperty('Country');
+                done();
+            }).catch(function (err) {
+                done(err);
+            })
     });
 
     it('inner join with reversed table', function (done) {
@@ -67,18 +68,7 @@ describe('Test join', function () {
     });
 
     it('three table join', function (done) {
-        con.select({
-            from: "Orders",
-            join: [{
-                with: "Customers",
-                type: "inner",
-                on: "Orders.CustomerID=Customers.CustomerID"
-            }, {
-                with: "Shippers",
-                type: "inner",
-                on: "Orders.ShipperID=Shippers.ShipperID"
-            }]
-        }).then(function (results) {
+        con.runSql(`select * from Orders join Customers on Orders.CustomerID=Customers.CustomerID inner join Shippers on Orders.ShipperID=Shippers.ShipperID`).then(function (results) {
             expect(results).to.be.an('array').length(196);
             const result = results[0];
             expect(result.CustomerID).to.be.an('number');
