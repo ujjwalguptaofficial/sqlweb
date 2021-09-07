68cd0c820081ad3785ab5580f27d5f5baeaf4035
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2021-09-07 09:37:14 +0530

    fix link issue

--- docs/src/components/menu.vue
+++ docs/src/components/menu.vue
@@ -9,7 +9,7 @@
         <i class="material-icons">menu</i>
       </button>
       <a class="b-menu__icon" href="/" title="JsStore Index Page">
-        <img src="/img/JsStore_145_64.png" alt="JsStore" />
+        <img src="img/JsStore_145_64.png" alt="JsStore" />
       </a>
     </template>
     <div v-else></div>
